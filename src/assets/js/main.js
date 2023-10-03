c66ad9985c96616c0d5696e1e64a3d52ced455ce
dulan nadeeja <dulannadeeja@gmail.com>
2023-10-03 15:02:58 +0530

    implemented 6 sliders using swiper js

---
+++ src/assets/js/main.js
@@ -0,0 +1,159 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    appInit();
+});
+
+function appInit() {
+    swiperInit();
+}
+
+
+// swiper slider init function
+function swiperInit() {
+    document.querySelectorAll('.swiper').forEach(function (el, i) {
+
+        let index = i + 1;
+        let isBrandsSlider = el.classList.contains('swiper-brands');
+        let isBestDealsSlider = el.classList.contains('swiper-best-deals');
+
+        if (isBrandsSlider) {
+            new Swiper(el, {
+
+                // Optional parameters
+                direction: 'horizontal',
+                slidesPerView: 4,
+                spaceBetween: 5,
+                slidesPerGroup: 4,
+                grabCursor: true,
+                keyboard: {
+                    enabled: true,
+                },
+
+                // pagination
+                pagination: {
+                    el: ".swiper-pagination" + '-' + index,
+                    clickable: true,
+                },
+
+                // Navigation arrows
+                navigation: {
+                    nextEl: '.swiper-button-next' + '-' + index,
+                    prevEl: '.swiper-button-prev' + '-' + index,
+                },
+
+
+                breakpoints: {
+                    640: {
+                        slidesPerView: 4,
+                        spaceBetween: 5,
+                        slidesPerGroup: 4,
+                    },
+                    768: {
+                        slidesPerView: 5,
+                        spaceBetween: 20,
+                        slidesPerGroup: 5,
+                    },
+                    1024: {
+                        slidesPerView: 6,
+                        spaceBetween: 20,
+                        slidesPerGroup: 6,
+                    },
+                    1280: {
+                        slidesPerView: 7,
+                        spaceBetween: 20,
+                        slidesPerGroup: 7,
+                    },
+                    1536: {
+                        slidesPerView: 8,
+                        spaceBetween: 20,
+                        slidesPerGroup: 8,
+                    },
+                }
+            });
+        } else if (isBestDealsSlider) {
+            console.log('best deals');
+            new Swiper(el, {
+
+                // Optional parameters
+                direction: 'vertical',
+                slidesPerView: 5,
+                spaceBetween: 20,
+                slidesPerGroup: 5,
+                grabCursor: true,
+                keyboard: {
+                    enabled: true,
+                },
+
+                // pagination
+                pagination: {
+                    el: ".swiper-pagination" + '-' + index,
+                    clickable: true,
+                },
+
+                // Navigation arrows
+                navigation: {
+                    nextEl: '.swiper-button-next' + '-' + index,
+                    prevEl: '.swiper-button-prev' + '-' + index,
+                },
+            });
+        } else {
+            new Swiper(el, {
+
+                // Optional parameters
+                direction: 'horizontal',
+                slidesPerView: 2,
+                spaceBetween: 20,
+                slidesPerGroup: 2,
+                grabCursor: true,
+                keyboard: {
+                    enabled: true,
+                },
+
+                // pagination
+                pagination: {
+                    el: ".swiper-pagination" + '-' + index,
+                    clickable: true,
+                },
+
+                // Navigation arrows
+                navigation: {
+                    nextEl: '.swiper-button-next' + '-' + index,
+                    prevEl: '.swiper-button-prev' + '-' + index,
+                },
+
+
+                breakpoints: {
+                    640: {
+                        slidesPerView: 2,
+                        spaceBetween: 20,
+                        slidesPerGroup: 2,
+                    },
+                    768: {
+                        slidesPerView: 3,
+                        spaceBetween: 20,
+                        slidesPerGroup: 3,
+                    },
+                    1024: {
+                        slidesPerView: 4,
+                        spaceBetween: 20,
+                        slidesPerGroup: 4,
+                    },
+                    1280: {
+                        slidesPerView: 5,
+                        spaceBetween: 20,
+                        slidesPerGroup: 5,
+                    },
+                    1536: {
+                        slidesPerView: 6,
+                        spaceBetween: 20,
+                        slidesPerGroup: 6,
+                    },
+                }
+            });
+
+        }
+
+
+    });
+}
+
